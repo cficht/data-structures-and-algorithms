488897a72dafff753e3aa86ee0ee9b4d04039bab
cficht <chris.ficht@gmail.com>
2020-03-09 16:57:38 -0700

    wrote and tested map function

--- challenges/arrayMap/array-map.js
+++ challenges/arrayMap/array-map.js
@@ -1,7 +1,12 @@
 const map = (arr, func) => {
     let newArr = [];
     for (let i = 0; i < arr.length; i++) {
-        newArr.push(func(arr[i]));
+        newArr[i] = func(arr[i]);
     }
     return newArr;
 };
+
+
+module.exports = {
+    map
+}
