3c27e9ba3f45b7f8e16fa942fc0d4690b7f34df8
cficht <chris.ficht@gmail.com>
2020-03-09 17:31:37 -0700

    commented on the array-map function

--- challenges/arrayMap/array-map.js
+++ challenges/arrayMap/array-map.js
@@ -1,9 +1,9 @@
 const map = (arr, func) => {
-    let newArr = [];
+    let newArr = []; // create new array
     for (let i = 0; i < arr.length; i++) {
-        newArr[i] = func(arr[i]);
+        newArr[i] = func(arr[i]); // pass in array[i] as the argument of the function and assign it to index i of the new array
     }
-    return newArr;
+    return newArr; // return the new array
 };
 
 module.exports = {
